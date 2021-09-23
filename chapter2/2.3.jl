8e7537fbddbe8f0eb7a9cc1afff0aea09dfc2bbc
Narges <m.dabiri48@gmail.com>
2021-09-23 22:56:58 +0330

    loops are included but it doesn't work yet

--- chapter2/2.3.jl
+++ chapter2/2.3.jl
@@ -1,24 +1,30 @@
 using Plots
 result_triangles=[[[0.0;0.0], [0.5;sqrt(3)/2],[1.0;0.0],[0.0;0.0]]]
-first_triangle=[]
-second_triangle=[]
-third_triangle=[]
-for i in 1:4
-    vertix=(result_triangles[1][i]-result_triangles[1][1])/2+result_triangles[1][1]
-    push!(first_triangle, vertix)
+for step in 1:3
+    first_triangle=[]
+    second_triangle=[]
+    third_triangle=[]
+    newOnes=[]
+    for triangle in result_triangles
+        for i in 1:4
+            vertix=(triangle[i]-triangle[1])/2+triangle[1]
+            push!(first_triangle, vertix)
+        end
+        for i in 1:4
+            push!(second_triangle, first_triangle[i]+(first_triangle[2]-first_triangle[1]))
+            push!(third_triangle, first_triangle[i]+(first_triangle[3]-first_triangle[1]))
+        end
+        append!(newOnes, first_triangle)
+        append!(newOnes, second_triangle)
+        append!(newOnes, third_triangle)
+        #println(result_triangles)
+    end
+    result_triangles=newOnes
 end
-for i in 1:4
-    push!(second_triangle, first_triangle[i]+(first_triangle[2]-first_triangle[1]))
-    push!(third_triangle, first_triangle[i]+(first_triangle[3]-first_triangle[1]))
-end
-insert!(result_triangles, 2, first_triangle)
-insert!(result_triangles, 3, second_triangle)
-insert!(result_triangles, 4, third_triangle)
-println(result_triangles)
 plot()
 for little_triangles in result_triangles
     #println(little_triangles)
-    plot!(hcat(little_triangles...)[1,:] , hcat(little_triangles...)[2,:])
+    plot!(hcat(little_triangles...)[1,:] , hcat(little_triangles...)[2,:], fill=(0,0.5,"orange"))
 end
 plot!()
 # plot(hcat(first_triangle...)[1,:],hcat(first_triangle...)[2,:])
