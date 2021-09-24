46f0f5c029255edb9f905e715279ca05c431c313
Narges <m.dabiri48@gmail.com>
2021-09-24 12:27:56 +0330

    serpenski triangles is complete!

--- chapter2/2.3.jl
+++ chapter2/2.3.jl
@@ -1,11 +1,11 @@
 using Plots
 result_triangles=[[[0.0;0.0], [0.5;sqrt(3)/2],[1.0;0.0],[0.0;0.0]]]
-for step in 1:3
-    first_triangle=[]
-    second_triangle=[]
-    third_triangle=[]
+for step in 1:5
     newOnes=[]
     for triangle in result_triangles
+        first_triangle=[]
+        second_triangle=[]
+        third_triangle=[]
         for i in 1:4
             vertix=(triangle[i]-triangle[1])/2+triangle[1]
             push!(first_triangle, vertix)
@@ -14,12 +14,13 @@ for step in 1:3
             push!(second_triangle, first_triangle[i]+(first_triangle[2]-first_triangle[1]))
             push!(third_triangle, first_triangle[i]+(first_triangle[3]-first_triangle[1]))
         end
-        append!(newOnes, first_triangle)
-        append!(newOnes, second_triangle)
-        append!(newOnes, third_triangle)
+        push!(newOnes, first_triangle)
+        push!(newOnes, second_triangle)
+        push!(newOnes, third_triangle)
         #println(result_triangles)
     end
     result_triangles=newOnes
+    #println(result_triangles)
 end
 plot()
 for little_triangles in result_triangles
