8049a09b0dd79d576e7a82cc6ddf7432df0e956e
Narges <m.dabiri48@gmail.com>
2021-09-26 23:20:29 +0330

    Khayyam triangle is done!

--- chapter2/2.4.jl
+++ chapter2/2.4.jl
@@ -1,9 +1,8 @@
 using Plots
 
 numberslist=[[1],[1,1]]
-#positions=[]
-#positions[1]=[0;0]
-for step in 3:6
+
+for step in 3:100
     R=[1]
     for n in 2:(step-1)
         number=numberslist[step-1][n-1]+numberslist[step-1][n]
@@ -13,5 +12,19 @@ for step in 3:6
     push!(numberslist,R)
 
 end
-println(numberslist)
 #println(numberslist)
+odd_num_pos=[]
+even_num_pos=[]
+y=0.0
+x=0.0
+for row in numberslist
+    x=y/2
+    for number in row
+        (number%2==0) ? (push!(even_num_pos, [x;y])) : (push!(odd_num_pos, [x;y]))
+        x+=1
+    end
+    y-=1
+end
+
+scatter(hcat(odd_num_pos...)[1,:], hcat(odd_num_pos...)[2,:], color=:green,  markersize=0.7)
+scatter!(hcat(even_num_pos...)[1,:], hcat(even_num_pos...)[2,:], color=:red, markersize=0.7)
