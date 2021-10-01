7417fa088ce6caf5624cdbd40a3fdfdc03a40a59
Narges <m.dabiri48@gmail.com>
2021-10-01 22:29:06 +0330

    mean hight is added

--- chapter3/RandomBallisticDeposition.jl
+++ chapter3/RandomBallisticDeposition.jl
@@ -1,7 +1,25 @@
 using Plots
+using Statistics
 arr=zeros((200,200))
 n=30000
 count=1
+meanList=[]
+function mean_calculater(arr,meanList)
+    hight=[]
+    for col in eachcol(arr)
+        count=0
+        for element in col
+            if element!=0
+                count+=1
+            end
+        end
+        push!(hight, count)
+    end
+    mean_num=mean(hight)
+    push!(meanList, mean_num)
+    return meanList
+end
+
 for i in 1:n
     column=rand(1:200)
     row=1
@@ -16,6 +34,8 @@ for i in 1:n
     if i%(10*200*count)==0
         count+=1
     end
+    meanList=mean_calculater(arr,meanList)
 end
 heatmap(hcat(arr), c=cgrad(:roma, 10, categorical = true, scale = :exp), xlabel="L")
 savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter3\\Fig\\RandomBallisticDeposition1.png")
+scatter!(1:n,meanList)
