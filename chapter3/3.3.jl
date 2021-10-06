ed2aa40db1c5549768fa2576e678252d340df0e8
Narges <m.dabiri48@gmail.com>
2021-10-06 20:22:01 +0330

    Random Ballistic Deposition With Relaxation is complete.

--- chapter3/3.3.jl
+++ chapter3/3.3.jl
@@ -1,71 +1,69 @@
 using Plots
 using CircularArrays
-n=30000 #the number of particles
-L=200 #Length of the line
+n=300 #the number of particles
+L=50 #Length of the line
 color=1 #used for color-change
-CircularCols=CircularArray(zeros(200)) #it makes a circular array for the columns
-MainArray=[CircularCols for i in 1:200] #it makes the whole systems scheme
-function particle_falling(MainArray, col, color) #this function operates falling of the particle
-    row=height_checker(MainArray, col)+1
-    MainArray[row][col]=count
+MainArray=zeros((L,L))
+
+function particle_falling(MainArray, col, L, color) #this function operates falling of the particle
+    row=height_checker(MainArray, col) #the particle stops at this height(row)
+    if row!=L
+        MainArray[row+1][col]=color
+    end
     return MainArray
 end
-function height_checker(MainArray, col) #comparing the hight of the column the particle exists in, with its neighbors
-    hight=height_calculater(MainArray, col)
-    if height[1]<height[2] && height[3]<height[2] #if it's own column is higher than others, there will be no holes.
-        the_row_num=hight[2]
-    end
-    if height[2]<height[1] || height[2]<height[3] #the other two neighbors' hights are higher.
+
+
+function height_checker(MainArray, col) #comparing the  of the column the particle exists in, with its neighbors
+    height=height_calculater(MainArray, col-1, [],L)
+    if height[2]<height[1] || height[2]<height[3] #the other two neighbors' heights are higher.
         if height[1]==height[3]
-            the_row_num=rand(height[1], height[3])
+            the_row_num=rand((height[1], height[3]))
         end
         if height[1]!=height[3]
-            the_row_num=max(height[1], height[3]) #if the two neighbors have equall hights, then the maximum hight will be chosen.
+            the_row_num=max(height[1], height[3]) #if the two neighbors have equall heights, then the maximum height will be chosen.
         end
+    else
+        the_row_num=height[2]
     end
     return the_row_num
 end
 
-function height_calculater(MainArray, col) #This function calculates the hight of the three columns we must know.
-    height=[]
-    for i in MainArray
-        for j in i
-            count=0
-            if i==(col-1)
-                for element in i
+function boundary_conditions(MainArray, c_index)
+    if MainArray
+        (c_index+2)=1
+    end
+    if (c_index+1)==1
+        c_index=L
+    end
+end
+function height_calculater(MainArray, c_index, height,L) #This function calculates the height of the three columns we must know.
+    for column in eachcol(MainArray)
+        while indexCounter<=3
+            if column==c_index
+                indexCounter=1
+                count=0
+                for element in column
                     if element!=0.0
                         count+=1
                     end
                 end
                 push!(height, count)
             end
-            count=0
-            if i==col
-                for element in i
-                    if element!=0.0
-                        count+=1
-                    end
-                end
-                push!(height, count)
-            end
-            count=0
-            if i==(col+1)
-                for element in i
-                    if element!=0.0
-                        count+=1
-                    end
-                end
-                push!(height, count)
-            end
+            c_index+=1
+            indexCounter+=1
         end
     end
-    return height #hight is a list of the hights[col-1, col, col+1]
+    #println(height)
+    return height # is a list of the heights[col-1, col, col+1]
 end
 
 for i in 1:n
     column= rand(1:L)
-    MainArray=particle_falling(MainArray,column,color)
+    MainArray=particle_falling(MainArray,column, L,color)
     if i%(10*200*color)==0
         color+=1
     end
 end
+
+heatmap(hcat(arr), c=cgrad(:roma, 10, categorical = true, scale = :exp), xlabel="L")
--- chapter3/BallisticDepositionWithRelaxation.jl
+++ chapter3/BallisticDepositionWithRelaxation.jl
@@ -1,86 +1,76 @@
 using Plots
-using CircularArrays
+using DataStructures
+using Statistics
 n=30000
 L=200
-CircularCols=CircularArray(zeros(200))
-MainArray=[CircularCols for i in 1:200]
-function particle_falling(MainArray,col,count)
+color=1
+meanList=[]
+arr=zeros((L,L))
+function BoundaryCondition(i)
+    if i==L+1
+        return 1
+    elseif i==0
+        return L
+    else
+        return i
+    end
+end
+function hight_cal(i,arr)
+    row=1
+    height=0
+    for j in 1:L
+        if arr[row,i]!=0.0
+            height+=1
+            row+=1
+        else
+            break #exit the loop for the next column
+        end
+    end
+    return height
+end
+function ColumnChoosing(arr, col)
+    h_dict=DataStructures.OrderedDict() #an ordered dictionary for the hights and columns
+    for i in (col-1):1:(col+1)
+        i=BoundaryCondition(i)
+        height= hight_cal(i,arr)
+        h_dict[height]=i
+        #setindex!(h_dict,i,height)
+    end
+    #println(h_dict)
+    hArr=collect(keys(h_dict)) #having all the heights in an array
+    return get(h_dict,min(hArr...), "ERROR")
+end
+function mean_calculater(arr,meanList)
+    hight=[]
+    for col in eachcol(arr)
+        count=0
+        for element in col
+            if element!=0
+                count+=1
+            end
+        end
+        push!(hight, count)
+    end
+    mean_num=mean(hight)
+    push!(meanList, mean_num)
+    return meanList
+end
+for particle in 1:n
+    column=rand(1:L)
+    col=ColumnChoosing(arr, column)
     row=1
     for j in 1:L
-        if MainArray[j][col]==0.0
-            MainArray[j][col]=count
+        if arr[row,col]==0.0
+            arr[row,col]=color
             break
         else
             row+=1
         end
     end
-    if i%(10*200*count)==0
-        count+=1
+    if particle%(10*200*color)==0
+        color+=1
     end
-    return MainArray
+    meanList=mean_calculater(arr,meanList)
 end
-function hight_calculater(MainArray, col) #This function calculates the hight of the three columns we must know.
-    for i in MainArray
-        hight=[]
-        for j in i
-            count=0
-            if i==(col-1)
-                for element in i
-                    if element!=0.0
-                        count+=1
-                    end
-                end
-                push!(hight, count)
-            end
-            count=0
-            if i==col
-                for element in i
-                    if element!=0.0
-                        count+=1
-                    end
-                end
-                push!(hight, count)
-            end
-            count=0
-            if i==(col+1)
-                for element in i
-                    if element!=0.0
-                        count+=1
-                    end
-                end
-                push!(hight, count)
-            end
-        end
-    end
-    return hight #hight is a list of the hights[col-1, col, col+1]
-end
-
-function hight_checker(MainArray, col)
-    hight=hight_calculater(MainArray, col)
-    if hight[2]<hight[1] && hight[2]<hight[3]
-        MainArray=particle_falling(MainArray, hight[2],1)
-    end
-    if hight[2]> hight[1] || hight[2]>hight[1]
-        if hight[1]!= hight[3]
-            MainArray=particle_falling(MainArray, min(hight[1],hight[3]),1)
-        end
-        if hight[1]== hight[3]
-            MainArray=particle_falling(MainArray, rand(hight[1], hight[3]),1)
-        end
-    end
-    return MainArray
-end
-for i in 1:n
-    column=rand(1:L)
-    MainArray=hight_checker(MainArray, column) #first we check which column is best to fall into, then which row, and then we have our new MainArray after each fall.
-end
-
-
-# println(B[1][6])
-# println(B[2][7])
-# for i in B
-#     for j in i
-#         B[j][6]=10
-#         println(B[j][6])
-#     end
-# end
+heatmap(hcat(arr), c=cgrad(:roma, 10, categorical = true, scale = :exp), xlabel="L")
+scatter!(1:n,meanList, xlabel="time", ylabel="mean hight of the layer")
