e83244b1cd82e90e2fb394111b0758ffff3ca38d
Narges <m.dabiri48@gmail.com>
2021-10-09 17:47:02 +0330

    suddenly height jumps to 201

--- chapter3/3.4.jl
+++ chapter3/3.4.jl
@@ -6,17 +6,17 @@ meanList=[]
 stdList=[]
 BigStdList=[]
 arr=zeros((L,L))
-function BoundaryCondition(i)
-    if i==L+1
-        return 1
-    elseif i==0
-        return L
+function BoundaryCondition(i, AvailableCols)
+    if i==AvailableCols[end]+1
+        return AvailableCols[1]
+    elseif i==AvailableCols[1]-1
+        return AvailableCols[end]
     else
         return i
     end
 end
-function height_cal(i,arr) #calculating the height of the layer in a column
-    i=BoundaryCondition(i)
+function height_cal(i,arr, AvailableCols) #calculating the height of the layer in a column
+    i=BoundaryCondition(i, AvailableCols)
     row=L #starting from the top layer
     height=L
     for j in 1:L
@@ -29,22 +29,23 @@ function height_cal(i,arr) #calculating the height of the layer in a column
     end
     return height
 end
-function neighbor_checking(arr, col)
+function neighbor_checking(arr, col, AvailableCols)
     h=[]
     for i in (col-1):1:(col+1)
-        height= height_cal(i,arr)
+        height= height_cal(i,arr, AvailableCols)
         if i==col
             push!(h, (height+1))
         else
             push!(h, height)
         end
     end
+    println("max:", maximum(h), h)
     return maximum(h)
 end
-function mean_and_std_calculater(arr,meanList,stdList,L)
+function mean_and_std_calculater(arr,meanList,stdList,L, AvailableCols)
     h=[]
     for i in 1:L
-        height= height_cal(i,arr)
+        height= height_cal(i,arr, AvailableCols)
         push!(h, height)
     end
     mean_num=mean(h)
@@ -59,21 +60,19 @@ function deposing(arr,L,n, color,meanList, stdList)
         arr[i, ceil(Int, L/2)]=-1
     end
     for particle in 1:n
-        col=rand(1:L)
-        if col in AvailableCols
-            height=neighbor_checking(arr, col)
-            if height>=L
-                break
-            end
-            arr[height, col]=color
-            if particle%(10*200*color)==0
-                color+=1
-            end
-            for i in col-1:1:col+1
-                for j in height+1:L
-                    arr[j, i]=-1
-                    push!(AvailableCols, i)
-                end
+        col=rand(AvailableCols)
+        height=neighbor_checking(arr, col, AvailableCols)
+        if height>=L
+            break
+        end
+        arr[height, col]=color
+        if particle%(10*200*color)==0
+            color+=1
+        end
+        for i in (col-1):1:(col+1)
+            for j in (height+1):L
+                arr[j, i]=-1
+                push!(AvailableCols, i)
             end
         end
         #meanList, stdList=mean_and_std_calculater(arr,meanList,stdList,L)
