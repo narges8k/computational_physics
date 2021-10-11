a0bb19909d8f83bf38d0ef4107e1360432d3d932
Narges <m.dabiri48@gmail.com>
2021-10-11 22:39:00 +0330

    Last Update

--- chapter3/3.4.jl
+++ chapter3/3.4.jl
@@ -36,15 +36,56 @@ function neighbor_checking(arr, col)
     end
     return maximum(h)
 end
+function width_cal(arr, L)
+    firstx=0
+    firsty=0
+    col=1 #starting from the beggining
+    for i in 1:L
+        for row in 1:L
+            if arr[row, col]!=0.0
+                firstx=col
+                firsty=row
+                break
+            end
+        end
+        if firstx!=0 && firsty!=0
+            break
+        else
+            col+=1
+        end
+    end
+    lastx=0
+    lasty=0
+    col=L #starting from the end
+    for i in 1:L
+        for row in 1:L
+            if arr[row, col]!=0.0
+                lastx=col
+                lasty=row
+                break
+            end
+        end
+        if lastx!=0 && lasty!=0
+            break
+        else
+            col-=1
+        end
+    end
+    d=sqrt((firstx-lastx)^2 + (firsty-lasty)^2)
+    return d
+end
 function deposing(L,n)
+    WidthList=[]
     color=1
     arr=zeros((L,L))
     meanList=[]
     stdList=[]
+    finalP=n
     for particle in 1:n
         col=rand(1:L)
         height=neighbor_checking(arr, col)
         if height>=L
+            finalP=particle
             break
         end
         if height!=1 || col==ceil(Int, L/2)
@@ -53,45 +94,13 @@ function deposing(L,n)
         if particle%(10*200*color)==0
             color+=1
         end
+        WidthNum=width_cal(arr, L)
+        push!(WidthList, WidthNum)
     end
-    return arr
+    return arr, WidthList, finalP
 end
-arr=deposing(L,30000)
-
-firstx=0
-firsty=0
-col=1 #starting from the beggining
-for i in 1:L
-    for row in 1:L
-        if arr[row, col]!=0.0
-            firstx=col
-            firsty=row
-            break
-        end
-    end
-    if firstx!=0 && firsty!=0
-        break
-    else
-        col+=1
-    end
-end
-lastx=0
-lasty=0
-col=L #starting from the end
-for i in 1:L
-    for row in 1:L
-        if arr[row, col]!=0.0
-            lastx=col
-            lasty=row
-            break
-        end
-    end
-    if lastx!=0 && lasty!=0
-        break
-    else
-        col-=1
-    end
-end
-diameter=sqrt((firstx-lastx)^2 + (firsty-lasty)^2)
+arr, WidthList, finalP=deposing(L,30000)
 heatmap(hcat(arr), c=cgrad(:roma, 10, categorical = true, scale = :exp), xlabel="L")
 savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter3\\Fig\\3.4.png")
+scatter(1:finalP, WidthList, xlabel="time",ylabel="width of the shrub" , markersize=0.001)
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter3\\Fig\\3.4_width.png")
