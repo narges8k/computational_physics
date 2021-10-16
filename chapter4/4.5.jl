2e9f680eb87252ab629e1151cd2097dc0c3a1ce8
Narges <m.dabiri48@gmail.com>
2021-10-16 19:59:02 +0330

    the whole code is written, but it has error.

--- chapter4/4.5.jl
+++ chapter4/4.5.jl
@@ -1,6 +1,5 @@
-using Plots,Statistics
+using Plots,Statistics,LaTeXStrings
 dim=10
-p=0.6
 function NeighborReturner(network_, i, j)
     neighbors=[]
     if j!=1 && network_[i,j-1]!=0
@@ -34,66 +33,88 @@ function percolation_check(network_,L,dim)
         return intersect(first_col,last_col)
     end
 end
-network_=zeros(Int, dim, dim)
-L=[]
-S=[]
-counter=1
-# for i in 1:dim
-#     network_[i,1]=1
-# end
-for col in 1:dim
-    for row in 1:dim
-        if p>rand()
-            neighbors=NeighborReturner(network_, row, col)
-            if length(neighbors)==0
-                network_[row,col]=counter
-                push!(L, counter)
-                push!(S, 1)
-                #println(S)
-                counter+=1
-            elseif length(neighbors)==1
-                network_[row,col]=LabelFinder(neighbors[1], L)
-                S[LabelFinder(neighbors[1], L)]+=1
-            elseif length(neighbors)==2 && LabelFinder(neighbors[1],L)==LabelFinder(neighbors[2],L)
-                network_[row,col]=LabelFinder(neighbors[1], L)
-                S[LabelFinder(neighbors[1], L)]+=1
-            else
-                S[LabelFinder(neighbors[1], L)]+=1+S[LabelFinder(neighbors[2], L)]
-                S[LabelFinder(neighbors[2], L)]=0
-                network_[row,col]=LabelFinder(neighbors[1], L)
-                L[neighbors[2]]=LabelFinder(neighbors[1], L)
+function percolation(dim,p)
+    network_=zeros(Int, dim, dim)
+    L=[]
+    S=[]
+    counter=1
+    for col in 1:dim
+        for row in 1:dim
+            if p>rand()
+                neighbors=NeighborReturner(network_, row, col)
+                if length(neighbors)==0
+                    network_[row,col]=counter
+                    push!(L, counter)
+                    push!(S, 1)
+                    #println(S)
+                    counter+=1
+                elseif length(neighbors)==1
+                    network_[row,col]=LabelFinder(neighbors[1], L)
+                    S[LabelFinder(neighbors[1], L)]+=1
+                elseif length(neighbors)==2 && LabelFinder(neighbors[1],L)==LabelFinder(neighbors[2],L)
+                    network_[row,col]=LabelFinder(neighbors[1], L)
+                    S[LabelFinder(neighbors[1], L)]+=1
+                else
+                    S[LabelFinder(neighbors[1], L)]+=1+S[LabelFinder(neighbors[2], L)]
+                    S[LabelFinder(neighbors[2], L)]=0
+                    network_[row,col]=LabelFinder(neighbors[1], L)
+                    L[neighbors[2]]=LabelFinder(neighbors[1], L)
+                end
             end
         end
     end
+    return network_,L,S
 end
-for j in  percolation_check(network_,L,dim)
-    S[j]=-1 # the infinit cluster is omitted from S in this way.
-end
-RadiusOfGyration_list=[]
-for  i in 1:length(S)
-    x_sum=0
-    y_sum=0
-    numerator=0
-    if S[i]!=0 && S[i]!=-1
-        for x in 1:dim
-            for y in 1:dim
-                 if network_[x, y]==i
-                    y_sum+=y
-                    x_sum+=x
+function RadiusOfGyration(network_, S)
+    for j in  percolation_check(network_,L,dim)
+        S[j]=-1 # the infinit cluster is omitted from S in this way.
+    end
+    RadiusOfGyration_list=[]
+    for  i in 1:length(S)
+        x_sum=0
+        y_sum=0
+        numerator=0
+        if S[i]!=0 && S[i]!=-1
+            for x in 1:dim
+                for y in 1:dim
+                     if network_[x, y]==i
+                        y_sum+=y
+                        x_sum+=x
+                    end
                 end
             end
-        end
-        y_com=y_sum/S[i]
-        x_com=x_sum/S[i]
-        for x in 1:dim
-            for y in 1:dim
-                 if network_[x, y]==i
-                    numerator+=sqrt((x-x_com)^2 + (y-y_com)^2)
+            y_com=y_sum/S[i]
+            x_com=x_sum/S[i]
+            for x in 1:dim
+                for y in 1:dim
+                     if network_[x, y]==i
+                        numerator+=sqrt((x-x_com)^2 + (y-y_com)^2)
+                    end
                 end
             end
-        end
 
-        push!(RadiusOfGyration_list,sqrt(numerator/S[i]))
+            push!(RadiusOfGyration_list,sqrt(numerator/S[i]))
+        end
     end
+    sum=0
+    counter=0
+    for i in RadiusOfGyration_list
+        sum+=i
+        counter+=1
+    end
+    return sum/counter
 end
-RadiusOfGyration_list
+
+
+Meanlist = []
+STDlist = []
+for p in 0.5:0.05:1
+    xi=[]
+    for run_num in 1:100
+        network_,L=percolation(dim,p)
+        push!(xi,RadiusOfGyration(network_, S))
+    end
+    push!(STDlist, std(xi))
+    push!(Meanlist, mean(xi))
+end
+scatter(0:0.05:1, Meanlist, yerr=STDlist, xlabel="P", ylabel=L"ξ", legend=false)
