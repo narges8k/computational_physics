8bd5e6df67c5bda064be037752147e6a57aa7a8b
Narges <m.dabiri48@gmail.com>
2021-10-18 02:33:50 +0330

    everything works correctly for the first step!

--- chapter4/4.7.jl
+++ chapter4/4.7.jl
@@ -15,32 +15,36 @@ function neighbors(network_, i,j, dim)
   end
   return neighbors_dict
 end
-function InitialNetwork(network_, dim,p)
-  neighbors_dict=neighbors(network_, rand(1:dim), rand(1:dim),dim)
-  println(neighbors_dict)#####
-  network_, OnEntries_dict=on_or_block(network_, dim, neighbors_dict,p)
-end
+
 function on_or_block(network_,dim,neighbors_dict ,p)
   OnEntries_dict=DataStructures.OrderedDict()
   for key in keys(neighbors_dict)
     if p>rand()
-      OnEntries_dict[key]=1
+      OnEntries_dict[key]=1 #On
     else
-      OnEntries_dict[key]=-1
+      OnEntries_dict[key]=-1 #Block
     end
+  end
     coordinate_arr=collect(keys(OnEntries_dict))
     for item in coordinat_arr
-      network_[item[1],item[2]]=OnEntries_dict[item]
+      network_[item[1],item[2]]=OnEntries_dict[item] #changing the attributes in network_, respectively
     end
-  return  network_,OnEntries_dict
+  return network_,OnEntries_dict
+end
+
+function InitialNetwork(network_, dim,p)
+  neighbors_dict=neighbors(network_, rand(1:dim), rand(1:dim),dim) #choosing a random entry and giving the neigbhors
+  println(neighbors_dict)#####
+  network_, OnEntries_dict=on_or_block(network_, dim, neighbors_dict,p)
+  return network, OnEntries_dict
 end
 
 dim=10
 p=0.7
 network_=zeros(Int,dim,dim)
-OnEntries=InitialNetwork(network_, dim,p)
-if length(OnEntries)>0
-  for entry in OnEntries
+network, OnEntries_dict=InitialNetwork(network_, dim,p)
+if length(OnEntries_dict)>0
+  for entry in OnEntries_dict
     neighbors_list=neighbors(network_,findall(x->x==10,l)[][1],findall(x->x==10,l)[][2], dim)
     for i in neighbors_list
       OnEntries=on_or_block(network_,dim,i )
