0e028e18af3470aa93ef4e4c0eba5e950f40369e
Narges <m.dabiri48@gmail.com>
2021-10-23 17:08:51 +0330

    coding process...(near to the end)

--- chapter5/4.2.jl
+++ chapter5/4.2.jl
@@ -15,13 +15,37 @@ function RandomWalk(TotalTime, probability)
     return cumsum(arr,dims=1)
 end
 run_num=100
+# a list cinsisting of lists to the number of probabilities,
+#each list having lists of each position in each run number per time:
 RandomWalkers=[[[] for j in 1:TotalTime] for i in 1:length(probability)]
 for n in 1:run_num
+    TheMatrix=RandomWalk(TotalTime, probability)
     col_num=1
-    for col in eachcol(RandomWalk(TotalTime, probability))
+    for col in eachcol(TheMatrix)
         for i in 1:length(col)
-            push!(RandomWalkers[col_num][i],RandomWalk(TotalTime, probability)[i,col_num])
+            push!(RandomWalkers[col_num][i],TheMatrix[i,col_num])
         end
         col_num+=1
     end
 end
+SavedData_mean=[[] for i in 1:length(probability)]
+for i in 1:length(RandomWalkers)
+    for j in RandomWalkers[i]
+        push!(SavedData_std[i],std(j))
+        push!(SavedData_mean[i],mean(j))
+    end
+end
+save("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.2\\chapter5_4.2_MeanAndStdData.jld",
+ "MeanData", SavedData_mean, "StdData", SavedData_std)
+ #plots:
+load("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.2\\chapter5_4.2_MeanAndStdData.jld")
+for i in 1:length(probability)
+    p=probability[i]
+    scatter!(1:TotalTime, SavedData_mean[i],title="<x> per time for different probabilities", xlabel="t", ylabel="<x>",label=L"%$p")
+end
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.2\\Figs\\MeanPlot.png")
+for i in 1:length(probability)
+    p=probability[i]
+    scatter!(1:TotalTime, SavedData_std[i],title="Var[x] per time  for different probabilities", xlabel="t", ylabel="<x>",label=L"%$p")
+end
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.2\\Figs\\VarPlot.png")
