d61f0264db6004956d323df0da8a7134154d94f2
Narges <m.dabiri48@gmail.com>
2021-10-23 15:33:07 +0330

    coding process...

--- chapter5/4.2.jl
+++ chapter5/4.2.jl
@@ -1,14 +1,29 @@
 using Plots, LaTeXStrings, JLD, Statistics
-time_step=200
-probability=[0.25,0.5,0.75]
-arr=zeros((time_step,length(probability)))
-for i in 1:time_step
-    for j in 1:length(probability)
-        if p>rand()
-            arr[i,j]=1
-        else
-            arr[i,j]=-1
+TotalTime=1000
+probability=[0.25,0.5,0.75,0.95]
+function RandomWalk(TotalTime, probability)
+    arr=zeros((TotalTime,length(probability)))
+    for i in 1:TotalTime
+        for j in 1:length(probability)
+            if p>rand()
+                arr[i,j]=1
+            else
+                arr[i,j]=-1
+            end
         end
     end
+    return cumsum(arr,dims=1)
 end
-cumsum(arr,dims=1)
+run_num=100
+RandomWlakers=[[] for i in 1:length(probability)]
+for n in 1:run_num
+    position_per_TimeStep_matrix=RandomWalk(TotalTime, probability)
+
+    col_num=1
+    for col in eachcol(position_per_TimeStep_matrix)
+        for i in col
+            push!(RandomWalkers[col_num],i)
+        end
+        col_num+=1
+    end
+end
