4cbbc248333c880815dd2eb3ef9a3c0da0c18b94
Narges <m.dabiri48@gmail.com>
2021-10-24 15:36:14 +0330

    coding process...

--- chapter5/4.4/4.4.jl
+++ chapter5/4.4/4.4.jl
@@ -1,18 +1,20 @@
 p=1/2
 q=1-p
 TotData=[[1.0]]
-steps=10
-for step in 2:steps
-    R=[TotData[step-1][1]*q] #first element
-    for i in 2:(2*step-2)
+
+step_=2
+while step_<10
+    R=[TotData[step_-1][1]*q] #first element
+    for i in 2:(2*step_-2)
         println(i)
         if i%2==0
             push!(R,0.0)
         else
-            push!(R, TotData[step-1][i]*q+TotData[step-1][i-2]*p)
+            push!(R, TotData[step_-1][i]*q+TotData[step_-1][i-2]*p)
         end
     end
-    push!(R,TotData[step-1][end]*p) #last element
+    push!(R,TotData[step_-1][end]*p) #last element
     push!(TotData,R)
+    step_+=1
 end
 TotData
