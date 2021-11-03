a6a2f9172c0152238d0d30fccd77e66cec46a103
Narges <m.dabiri48@gmail.com>
2021-11-04 01:21:04 +0330

    coding process...

--- chapter5/4.6/4.6.jl
+++ chapter5/4.6/4.6.jl
@@ -20,15 +20,29 @@ function RandomWalk(arr, L, direction_list,first_pos)
     while true
         push!(direction_choosing, rand(direction_list))
         path=cumsum(direction_choosing,dims=1)
-        if path[end][1]>first_pos+5 # if going out of the second boundary, neglect the particle
+        if path[end][1]>(first_pos[1]+5) # if going out of the second boundary, neglect the particle
             return 0 #ZONE OUT
             break
         elseif length(findall(x->x ∉ -3:0, arr[path[end][1]-1:2:path[end][1]+1, path[end][2]-1:2:path[end][2]+1]))>0
-            path[end]-=1
-            return path[end] #SUSCCESSFUL COLLISION, returning the current position
+            arr[path[end][1], path[end][2]]-=1
+            return path[end] #SUSCCESSFUL COLLISION, returning the final_destination
             break
         end
     end
 end
 
 direction_list=[[0,1],[0,-1],[1,0],[-1,0],[-1,1],[1,-1],[1,1],[-1,-1]]
+L=20
+arr=zeros((L,L))
+N=10
+color=1
+for particle in 1:N
+    peak=height_cal(arr)
+    f_d=RandomWalk(arr, L, direction_list,[peak+2, rand(1:L)])
+    if arr[f_d[1],f_d[2]]==-3
+        arr[f_d[1],f_d[2]]=color
+    end
+    if particle%(10*200*color)==0
+        color+=1
+    end
+end
