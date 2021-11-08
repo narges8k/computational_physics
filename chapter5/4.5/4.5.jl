90989830693f5cf232dcc23cd5a1bcf0e87b41bf
Narges <m.dabiri48@gmail.com>
2021-11-08 21:19:15 +0330

    All Done

--- chapter5/4.5/4.5.jl
+++ chapter5/4.5/4.5.jl
@@ -4,13 +4,17 @@ function movement(first_pos, step_num)
     walking=rand(directions, step_num)
     return first_pos .+ cumsum(hcat(walking),dims=1)[step_num]
 end
-function RadiusOfGyration(data)
+function Rg(data)
+    COM=(mean(data, dims=1))
+    return sqrt(mean((data[:,1] .- COM[1]) .^2 + (data[:,2] .- COM[2]) .^2))
+end
 
 step_num_list=[i for i in 100:100:1000]
 first_pos=[0,0]
-run_num=10000
+run_num=100000
 AllData=[]
 MeanSquaredDistance=[]
+RgData=[]
 for step_num in step_num_list
     final_destinations=zeros(Int, run_num, 2)
     squared_distance=[]
@@ -20,23 +24,16 @@ for step_num in step_num_list
     end
     push!(SavedData, final_destinations)
     push!(MeanSquaredDistance, mean(squared_distance))
-    end
-
-scatter(MeanSquaredDistance, step_num_list, ylabel=L"<r^2>", xlabel="step",
-    title="Mean Squared Distance over steps", legend=false, dpi=400)
-save("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.5\\2DWalker.jld",
-    "data", AllData, "MeanSquaredDistanceData",MeanSquaredDistance )
-savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.5\\Figs\\MeanSquare(r)_step.png")
-
-function Rg(data)
-    COM=(mean(data, dims=1))
-    return sqrt(mean((data[:,1] .- COM[1]) .^2 + (data[:,2] .- COM[2]) .^2))
+    push!(RgData, Rg(final_destinations))
 end
+#Radius of Gyration plot:
+scatter(log.(RgData), log.(step_num_list), legend=false, xlabel="log(Time)", ylabel="log(Rg)",
+    title="Logarithm of Radious of Gyration over the Logarithm of Time", dpi=400, titlefontsize=9)
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.5\\Figs\\Rg_Time.png")
+# mean(r^2) plot:
+scatter(MeanSquaredDistance, step_num_list, ylabel=L"<r^2>", xlabel="Time",
+    title="Mean Squared Distance over Time", legend=false, dpi=400)
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.5\\Figs\\MeanSquare(r)_Time.png")
 
-
-
-# p=SavedData[1]
-# (p[:,1] .- com[1])
-# (p[:,1] .- com[1]).^2
-# length(eachrow(p))
-# com=mean(p,dims=1)
+save("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.5\\2DWalker.jld",
+    "data", AllData, "MeanSquaredDistanceData",MeanSquaredDistance, "RgData",RgData )
