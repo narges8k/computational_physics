0457e48bc9f095a0e6c918f0d68868dbe8b95d22
Narges <m.dabiri48@gmail.com>
2021-11-10 17:45:39 +0330

    some changes made to make it right yet I don't see any change

--- chapter5/4.6/4.6.jl
+++ chapter5/4.6/4.6.jl
@@ -1,4 +1,4 @@
-using Plots, Statistics, JLD, LaTeXStrings
+using ProgressBars,Plots, Statistics, JLD, LaTeXStrings
 function Boundary(i,L)
     if i>L
         return i-L
@@ -22,61 +22,54 @@ function height_cal(arr)
     return maximum(h)
 end
 
-function RandomWalk(Network_, L, FirstPos, seperation, DirectionList)
+function RandomWalk(Network_, L, FirstPos, t_limit, DirectionList)
     CurrentPos=FirstPos
     #println("firstPos:", FirstPos)
     while true
-        CurrentPos .+=rand(DirectionList)
-        CurrentPos[2]=Boundary(CurrentPos[2],L)
-        #println("currentpos:",CurrentPos)
-        if CurrentPos[1]>L
-            return Network_, -4
-        elseif CurrentPos[1]> (FirstPos[1]+seperation)
-            return Network_, 0
-        elseif CurrentPos[1]==1
-            Network_[CurrentPos[1],CurrentPos[2]]-=1
-            return Network_, CurrentPos
-        elseif CurrentPos[1]==L
-            if length(findall(x->x ∉ -2:0,vcat(Network_[CurrentPos[1]-1,CurrentPos[2]],
-                                Network_[CurrentPos[1], Boundary(CurrentPos[2]-1,L):2:Boundary(CurrentPos[2]+1,L)]...)))>0
-                Network_[CurrentPos[1],CurrentPos[2]]-=1
+        temp=CurrentPos .+ rand(DirectionList)
+        temp= [temp[1],Boundary(temp[2],L)]
+        if Network_[temp...]<=0
+            CurrentPos=temp
+            if CurrentPos[1]==1
+                Network_[CurrentPos...]-=1
                 return Network_, CurrentPos
-            else
+            elseif CurrentPos[1]> t_limit
                 return Network_, 0
+            elseif length(findall(x->x>0,vcat(Network_[CurrentPos[1]-1:2:CurrentPos[1]+1,CurrentPos[2]],
+                            Network_[CurrentPos[1], Boundary(CurrentPos[2]-1,L):2:Boundary(CurrentPos[2]+1,L)]...)))>0
+                    Network_[CurrentPos...]-=1
+                return Network_,CurrentPos
+            else
+                continue
             end
-        elseif length(findall(x->x>0,
-                        vcat(Network_[CurrentPos[1]-1:2:CurrentPos[1]+1,CurrentPos[2]],
-                                Network_[CurrentPos[1], Boundary(CurrentPos[2]-1,L):2:Boundary(CurrentPos[2]+1,L)]...)))>0
-                Network_[CurrentPos[1],CurrentPos[2]]-=1
-            return Network_,CurrentPos
         else
-            continue
+            return Network_,CurrentPos
         end
     end
 end
 direction_list=[[0,1],[0,-1],[1,0],[-1,0]]
-L=50
+L=200
 Network_=zeros(Int,L,L)
-N=3000
+N=80000
 color=1
-separation=5
-for particle in 1:N
-    #println("particle:", particle)
-    peak=height_cal(Network_)
-    Network_,FinalPos=RandomWalk(Network_, L, [peak+3, rand(1:L)], separation, direction_list)
-    #println("the final position:", f_d)
-    if FinalPos==-4
+seperation=5
+for particle in ProgressBar(1:N)
+    b_limit=height_cal(Network_) + 3
+    t_limit= b_limit+seperation
+    if t_limit<L
+        Network_,FinalPos=RandomWalk(Network_, L, [b_limit, rand(1:L)], t_limit, direction_list)
+        if FinalPos==0
+            continue
+        elseif Network_[FinalPos...]==-3
+            Network_[FinalPos...]=color
+        end
+        if particle%(10*L*color)==0
+            color+=1
+        end
+    else
         break
-    elseif FinalPos==0
-        continue
-    elseif Network_[FinalPos[1],FinalPos[2]]==-3
-        Network_[FinalPos[1],FinalPos[2]]=color
-    end
-    if particle%(10*20*color)==0
-        color+=1
     end
 end
 Network_[findall(x->x<0, Network_)].=0
-Network_
-p1=heatmap(Network_)
-p2=heatmap(Network_)
+heatmap(Network_, dpi=400)
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter5\\4.6\\Figs\\4.6_strange.png")
