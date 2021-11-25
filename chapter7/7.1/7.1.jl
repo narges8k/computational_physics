698080026dd5dd91d88194360423ad36670eb14d
Narges <m.dabiri48@gmail.com>
2021-11-25 16:33:36 +0330

    IMC included but! there's sth I don't understand.

--- chapter7/7.1/7.1.jl
+++ chapter7/7.1/7.1.jl
@@ -6,8 +6,15 @@ function SMC(f :: Function, a :: Real, b :: Real, N :: Integer )
     error=std(f₍ₓ₎)/sqrt(N)
     return ∫f₍ₓ₎dx, error
 end
-function IMC()
+function IMC(f :: Function, a :: Real, b :: Real, g₍ₓ₎ :: Function, y :: Function, N :: Integer)
+    f₍ₓ₎/g₍ₓ₎ = x-> f(x)/g(x)
+    NewD= (f₍ₓ₎/g₍ₓ₎).(y.rand(N))
+    ∫g₍ₓ₎dx= 1-ℯ^2
+    ∫f₍ₓ₎dx= ∫g₍ₓ₎dx * mean(NewD)
+    error= std(NewD)/sqrt(N)
+    return ∫f₍ₓ₎dx, error
 
+end
 
-    
 SMC(x->ℯ^(-x^2), 0, 2, 10^6)
+IMC(x->ℯ^(-x^2), 0, 2, x -> ℯ^(-x), x -> -log(1-x) , 10^6)
