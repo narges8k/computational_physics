c335a1418a6403b34f3865159f8cfb12ed0d618b
Narges <m.dabiri48@gmail.com>
2021-11-28 12:30:02 +0330

    All Done

--- chapter7/7.1/7.1.jl
+++ chapter7/7.1/7.1.jl
@@ -9,16 +9,16 @@ function SSMC(f :: Function, a :: Real, b :: Real, N :: Integer )
     return ∫f₍ₓ₎dx, error
 end
 
-function ISMC(f :: Function, a :: Real, b :: Real, g :: Function, y :: Function, N :: Integer)
+function ISMC(f :: Function, g :: Function, y :: Function, a :: Real, b :: Real, N :: Integer)
     drivation= x-> f(x)/g(x)
     samples= drivation.(y.(rand(N)))
-    ∫g₍ₓ₎dx= 1-ℯ^2
+    ∫g₍ₓ₎dx= 1-ℯ^(-2)
     ∫f₍ₓ₎dx= ∫g₍ₓ₎dx * mean(samples)
     error= ∫g₍ₓ₎dx * std(samples)/sqrt(N)
     return ∫f₍ₓ₎dx, error
 end
 
-nList=Integer.([10^n for n in 1:6])
+nList=Integer.([10^n for n in 1:8])
 SSMC_res=[]
 prog= Progress(length(nList))
 n=0
@@ -32,15 +32,20 @@ ISMC_res=[]
 n=0
 prog= Progress(length(nList))
 for n in nList
-    I, error=ISMC(x->ℯ^(-x^2), 0, 2, x -> ℯ^(-x), x -> -log(1-(1-ℯ^2)*x), n)
-    bench = @benchmark ISMC(x->ℯ^(-x^2), 0, 2, x -> ℯ^(-x), x -> -log(1-(1-ℯ^2)*x), n)
+    I, error=ISMC(x->ℯ^(-x^2), x -> ℯ^(-x), x -> -log(1-(1-ℯ^(-2))*x), 0, 2, n)
+    bench = @benchmark ISMC(x->ℯ^(-x^2), x -> ℯ^(-x), x -> -log(1-(1-ℯ^(-2))*x), 0, 2, n)
     push!(ISMC_res, (I, error, mean(bench).time, bench))
     next!(prog)
 end
-
+save("../../chapter7/7.1/data.jld","ISMC", ISMC_res, "SSMC", SSMC_res, "samples", nList)
 AnswerList=[Integral_answer for i in 1: length(nList)]
-plot(log.(nList), [getindex.(SSMC_res, 1) getindex.(ISMC_res, 1) AnswerList])
-
-plot(log.(nList), [getindex.(SSMC_res, 2) getindex.(ISMC_res, 2)])
-
-plot(nList, )
+scatter(log.(nList), [getindex.(SSMC_res, 1) getindex.(ISMC_res, 1) AnswerList], markercolor=[:blue :purple :orange], label=nothing)
+plot!(log.(nList), [getindex.(SSMC_res, 1) getindex.(ISMC_res, 1) AnswerList], linecolor=[:blue :purple :orange],
+    dpi=600,labels=["SSMC" "ISMC" "Absolute Result"], ylabel="Integral", xlabel="Number of Samples",
+        title=L"Monte\ Carlo\ Results\ for\ I= \int{}_{0}^{2}\ e^{-x^2}\ dx" )
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter7\\7.1\\Figs\\ResultsPlot.png")
+scatter(log.(nList), [getindex.(SSMC_res, 2) getindex.(ISMC_res, 2)], markercolor=[:blue :purple], label= nothing)
+plot!(log.(nList), [getindex.(SSMC_res, 2) getindex.(ISMC_res, 2)],
+     labels=["SSMC" "ISMC"], xlabel="Number of Samples", ylabel=" Error", dpi=600, linecolor=[:blue :purple],
+        title=L"Monte\ Carlo\ Errors\ of\ the\ Results\ for\ I= \int{}_{0}^{2}\ e^{-x^2}\ dx")
+savefig("C:\\Users\\Narges\\Documents\\GitHub\\computational_physics\\chapter7\\7.1\\Figs\\ErrorPlot.png")
