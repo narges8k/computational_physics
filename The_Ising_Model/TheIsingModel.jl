3b27d184c10c9e38e98c1fd73c65681a43e480eb
Narges <m.dabiri48@gmail.com>
2021-12-09 20:02:46 +0330

    keeping up with it

--- The_Ising_Model/TheIsingModel.jl
+++ The_Ising_Model/TheIsingModel.jl
@@ -1,19 +1,56 @@
-using Plots,LaTeXStrings, Statistics, StatsPlots, JLD, ProgressBars, StatsBase
+using Plots,LaTeXStrings, Statistics, StatsPlots, JLD, ProgressBars, StatsBase, Ranges
 
 function Initials()
     flag=0
     L=10
-    T=1
+    β=range(0.1,0.9, 80)
+    N=L
     system=rand([-1,1], L, L) 
 end
 
 function DE(i, j, system)
-    return -2*system[i,j](system[i-1,j]+system[i,j-1]+system[i,j+1], system[i+1,j])
+    return system[i,j](system[i-1,j]+system[i,j-1]+system[i,j+1], system[i+1,j])
 end
 
-function MC()
-    i=rand(1:L);j=rand(1:L)
-    ΔE= DE(i,j)
-    if ΔE<0
-        
+function successful_change(system, i, j, L, β)
+    ΔE_sum=0
+    system[i,j]=ΔE
+    ΔE_sum += ΔE
+    return system, ΔE_sum
 end
+
+function MC(system,L,N,β)
+    for n in 1:N
+        i=rand(1:L);j=rand(1:L)
+        ΔE= DE(i,j)
+        if ΔE<=0 || rand()<exp(-ΔE*β)
+            system,ΔE_sum, M=successful_change(system, i, j)
+        end
+    end
+    return system, ΔE_sum
+end
+
+function Inital_system(system,L)
+    β=0.1
+    N=100*L^2
+    for n in 1:N
+        i=rand(1:L);j=rand(1:L)
+        ΔE= DE(i,j)
+        if ΔE<=0 || rand()<exp(-ΔE*β)
+            system[i,j]=ΔE
+    end
+    E=0
+    for i in 1:L
+        for j in 1:L
+            E+=
+        end
+    end
+    return system
+end
+
+system=Initial_system(system, L)
+
+for β in range(0.11, 0.9, 80)
+    N=10*L^2
+    system, ΔE_sum = MC(system,L,N,β)
+    
