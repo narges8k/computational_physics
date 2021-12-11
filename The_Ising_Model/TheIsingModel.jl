9b667d4bac5319a2b02aff4b9efff0e5f305ed18
Narges <m.dabiri48@gmail.com>
2021-12-11 17:43:49 +0330

    still going

--- The_Ising_Model/TheIsingModel.jl
+++ The_Ising_Model/TheIsingModel.jl
@@ -1,11 +1,9 @@
 using Plots,LaTeXStrings, Statistics, StatsPlots, JLD, ProgressBars, StatsBase, Ranges
 
-function Initials()
-    flag=0
-    dim=4
-    β=range(0.1,0.9, 80)
-    System=rand([-1,1], dim, dim) 
-end
+#Initials
+dim=4
+β=range(0.1,0.9, 80)
+System=rand([-1,1], dim, dim) 
 
 function FindEnergy(ُSystem::Matrix{Int8}, dim::Integer)
     E = 0.0
@@ -18,16 +16,11 @@ function FindEnergy(ُSystem::Matrix{Int8}, dim::Integer)
     return E
 end
 
-function DE(i, j, System)
-    return system[i,j](System[i-1,j]+System[i,j-1]+System[i,j+1], System[i+1,j])
+function DE(System::Matrix{Int8}, i::Integer, j::Integer)
+    return system[i,j]*(System[i-1,j]+System[i,j-1]+System[i,j+1], System[i+1,j])
 end
 
-function successful_change(System,spin, ΔE, ΔE_sum)
-
-    return System, ΔE_sum, -spin
-end
-
-function MC(System,i,j,dim,N,β)
+function MC(System::Matrix{Int8}, dim::Integer, N::Integer, β::Real)
     ΔE_sum=0
     ΔM_sum=0
     for n in 1:N
@@ -35,37 +28,37 @@ function MC(System,i,j,dim,N,β)
         System[i,j]=spin
         ΔE= DE(i,j)
         if ΔE<=0 || rand()<exp(-ΔE*β)
-            System[i,j] = -System[i,j]
+            System[i,j] = -spin
             ΔE_sum += ΔE
-            ΔM_sum += System[i,j]
+            ΔM_sum += 2*(-spin)
         end
     end
     return System, ΔE_sum, ΔM_sum
 end
 
-function Inital_system(system,dim)
+function Initial_System(System::Matrix{Int8},dim::Integer)
     β=0.1
     N=100*dim^2
     for n in 1:N
         i=rand(1:dim);j=rand(1:dim)
         ΔE= DE(i,j)
         if ΔE<=0 || rand()<exp(-ΔE*β)
-            System[i,j]=ΔE
+            System[i,j]= -System[i,j]
+        end
     end
     return System
 end
 
 System=Initial_System(System, dim)
-
+M_list=[]; E_list=[]
 for β in range(0.11, 0.9, 80)
     Eᵢ=FindEnergy(system, dim)
     Mᵢ=sum(System)
     N=10*dim^2
-    System, ΔE, ΔM = MC(System, i,j ,dim,N,β)
+    System, ΔE, ΔM = MC(System,dim,N,β)
     Eᵢ += ΔE 
     Mᵢ += ΔM
-    M_list=[]; E_list=[]
-    System, ΔE, ΔM = MC(System, i,j ,dim,N,β)
+    System, ΔE, ΔM = MC(System,dim,N,β)
     E = Eᵢ + ΔE
     M = Mᵢ + ΔM
     push!(E_list, E)
