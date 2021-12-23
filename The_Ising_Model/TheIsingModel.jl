02a092318a82a3fa099f9ccca8e7eb66f6f014e0
Narges <m.dabiri48@gmail.com>
2021-12-23 11:26:08 +0330

    great changes made

--- The_Ising_Model/TheIsingModel.jl
+++ The_Ising_Model/TheIsingModel.jl
@@ -1,9 +1,4 @@
-using Plots,LaTeXStrings, Statistics, StatsPlots, JLD, ProgressBars, StatsBase, Ranges
-
-#Initials
-dim=4
-β=range(0.1,0.9, 80)
-System=rand([-1,1], dim, dim) 
+using Plots, LaTeXStrings, Statistics, StatsPlots, JLD, ProgressBars, StatsBase, Ranges
 
 function FindEnergy(System::Matrix{Int8}, dim::Integer)
     E = 0.0
@@ -13,57 +8,69 @@ function FindEnergy(System::Matrix{Int8}, dim::Integer)
             E += -sum(System[i, j] * System[Neighbors]) / 2
         end
     end
-    return E
+    return E / dim^2
 end
 
 function DE(System::Matrix{Int8}, i::Integer, j::Integer)
     Neighbors = [[1 + i % dim, j], [i, 1 + j % dim], [(-2 + i + dim) % dim + 1, j], [i, (-2 + j + dim) % dim + 1]]
-    return system[i,j]*sum(System[CartesianIndex.(Tuple.(Neighbors))])
+    return system[i, j] * sum(System[CartesianIndex.(Tuple.(Neighbors))])
 end
 
 function MC(System::Matrix{Int8}, dim::Integer, N::Integer, β::Real)
-    ΔE_sum=0
-    ΔM_sum=0
-    for n in 1:N
-        i=rand(1:dim);j=rand(1:dim)
-        System[i,j]=spin
-        ΔE= DE(i,j)
-        if ΔE<=0 || rand()<exp(-ΔE*β)
-            System[i,j] = -spin
+    ΔE_sum = 0
+    ΔM_sum = 0
+    for _ = 1:N
+        i = rand(1:dim)
+        j = rand(1:dim)
+        System[i, j] = spin
+        ΔE = DE(System, i, j)
+        if ΔE <= 0 || rand() < exp(-ΔE * β)
+            System[i, j] = -spin
             ΔE_sum += ΔE
-            ΔM_sum += 2*(-spin)
+            ΔM_sum += 2 * (-spin)
         end
     end
     return System, ΔE_sum, ΔM_sum
 end
 
-function Initial_System(System::Matrix{Int8},dim::Integer)
-    β=0.1
-    N=100*dim^2
-    for n in 1:N
-        i=rand(1:dim);j=rand(1:dim)
-        ΔE= DE(i,j)
-        if ΔE<=0 || rand()<exp(-ΔE*β)
-            System[i,j] *= -1
-        end
+function ising2d(dim::Integer, β::Real, MCS::Real, N::Integer)
+    System = rand([Int8(-1), Int8(1)], dim, dim)
+
+    Eᵢ = FindEnergy(system, dim)
+    Mᵢ = mean(System)
+    M_list = [Eᵢ]
+    E_list = [Mᵢ]
+    for ـ = 1:MCS
+        System, ΔE, ΔM = MC(System, dim, N, β)
+        E = E_list[end] + ΔE
+        M = M_list[end] + ΔM
+        push!(E_list, E)
+        push!(M_list, M)
     end
-    return System
+    return mean(E_list), mean(M_list), β^2 * var(E_list[ceil(Int64, MCS / 5):end]) * dim^2, β * var(E_list[ceil(Int64, MCS / 5):end])
 end
 
-System=Initial_System(System, dim)
-M_list=[]; E_list=[]
-for β in range(0.11, 0.9, 80)
-    Eᵢ=FindEnergy(system, dim)
-    Mᵢ=sum(System)
-    N=10*dim^2
-    System, ΔE, ΔM = MC(System,dim,N,β)
-    Eᵢ += ΔE 
-    Mᵢ += ΔM
-    System, ΔE, ΔM = MC(System,dim,N,β)
-    E = Eᵢ + ΔE
-    M = Mᵢ + ΔM
+#Initials
+dim = 4
+β_list = range(0.1, 0.9, 80)
+
+E_list = []
+M_list = []
+C_list = []
+χ_list = []
+
+for β in β_list
+    E, M, C, χ = ising2d(dim, β, 1000, 100)
     push!(E_list, E)
     push!(M_list, M)
+    push!(C_list, C)
+    push!(χ_list, χ)
 end
 
+plot(β_list, E_list)
 
+plot(β_list, M_list)
+
+plot(β_list, C_list)
+
+plot(β_list, χ_list)
