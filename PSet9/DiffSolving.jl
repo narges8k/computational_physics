92bd3dbfa24f263c554855fbda6c1a7e2085d071
Narges <m.dabiri48@gmail.com>
2022-01-05 16:48:14 +0330

    update

--- PSet9/DiffSolving.jl
+++ PSet9/DiffSolving.jl
@@ -1,11 +1,11 @@
 module DiffSolving
 
-export EulerMethod
+export EulerMethod, InstableAlgorithm, EulerCromer, Verlet, VelocityVerlet
 
 using LaTeXStrings, Plots, ProgressBars, Statistics, StatsBase
 cd(dirname(@__FILE__))
 #First Order ODE_Comparison between Euler method's and Numerical solution
-#RC Circuit
+
 function EulerMethod(func::Function, xᵢₙᵢₜ::Vector{Float64}, tₘᵢₙ::Float64, tₘₐₓ::Float64, h::Float64)
     t_range=collect(range(tₘᵢₙ, tₘₐₓ, step=h))
     Q=zeros(length(t_range), length(xᵢₙᵢₜ))
@@ -31,50 +31,50 @@ end
 
 #Second Order ODE
 #Simple Harmonic Oscillator
-function eulercromer(func::Function, xᵢₙᵢₜ::Float64, vᵢₙᵢₜ::Float64, tₘᵢₙ::Float64, tₘₐₓ::Float64, h::Float64)
-    t_range=collect(range(tₘᵢₙ, tₘₐₓ, step=h))
-    x=zeros(length(t_range))
-    v=zeros(length(t_range))
+function EulerCromer(func::Function, xᵢₙᵢₜ::Float64, vᵢₙᵢₜ::Float64, tₘᵢₙ::Float64, tₘₐₓ::Float64, h::Float64)
+    t=collect(range(tₘᵢₙ, tₘₐₓ, step=h))
+    x=zeros(length(t))
+    v=zeros(length(t))
 
     v[1]= vᵢₙᵢₜ
     x[1]= xᵢₙᵢₜ
-    for i in 1:length(t_range)-1
+    for i in 1:length(t)-1
         v[i+1] = v[i] + h * func(t[i], x[i])
         x[i+1] = x[i] + h * v[i]
     end
     
-    return t_range, x, v
+    return t, x, v
 end
 
 function Verlet(func::Function, xᵢₙᵢₜ::Float64, vᵢₙᵢₜ::Float64, tₘᵢₙ::Float64, tₘₐₓ::Float64, h::Float64)
-    t_range=collect(range(tₘᵢₙ, tₘₐₓ, step=h))
-    x=zeros(length(t_range))
-    v=zeros(length(t_range))
+    t=collect(range(tₘᵢₙ, tₘₐₓ, step=h))
+    x=zeros(length(t))
+    v=zeros(length(t))
 
     v[1]= vᵢₙᵢₜ
     x[1]= xᵢₙᵢₜ
 
-    for i in 2: legnth(t_range)-1
+    for i in 2: length(t)-1
         x[i+1]= 2 * x[i] - x[i-1] + func(t[i], x[i]) * h^2
         v[i+1]= (x[i+1] = x[i])/h
     end
-    return t_range, v, x
+    return t, v, x
 end
 
 function VelocityVerlet(func::Function, xᵢₙᵢₜ::Float64, vᵢₙᵢₜ::Float64, tₘᵢₙ::Float64, tₘₐₓ::Float64, h::Float64)
-    t_range= collect(range(tₘᵢₙ, tₘₐₓ, step=h))
-    x=zeros(length(t_range))
-    v=zeros(length(t_range))
+    t= collect(range(tₘᵢₙ, tₘₐₓ, step=h))
+    x=zeros(length(t))
+    v=zeros(length(t))
 
     v[1]= vᵢₙᵢₜ
     x[1]= xᵢₙᵢₜ
 
-    for i in 2:length(t_range)-1
+    for i in 2:length(t)-1
         x[i+1] = x[i] + v[i] * h + 1/2 * func(t[i], x[i]) * h^2
         v[i+1] = v[i] + 1/2 * (func(t[i], x[i]) + func(t[i+1], x[i+1])) * h
     end
     
-    return t_range, x, v
+    return t, x, v
 end
 
 end
