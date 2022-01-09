2325873e9d6bc98058afcd0884281425fd45e174
Narges <m.dabiri48@gmail.com>
2022-01-09 22:30:25 +0330

    All Done

--- PSet9/Bifurcation.jl
+++ PSet9/Bifurcation.jl
@@ -1,4 +1,4 @@
-using Plots, LaTeXStrings
+using Plots, LaTeXStrings, ProgressBars
 cd(dirname(@__FILE__))
 function LogisticMap(x₀::Vector{Float64} , r::Float64, n::Real)
     x = x₀
@@ -19,7 +19,7 @@ scatter(r_s, x_s, legend=false, ms=0.5, color=:black, dpi=400, xlabel=L"r", ylab
 savefig("../../computational_physics/PSet9/Figs/Bifurcation.pdf")
 
 ####Part B####
-###Zoomed Plot####
+####Zoomed Plot####
 r_zoom= collect(range(0.70, 1.0, step=10^(-4)))
 x₀_zoom = rand(range(0.70, 1.0, length=100), 100)
 x_zoom=zeros(length(r_zoom), length(x₀_zoom))
@@ -29,3 +29,28 @@ end
 scatter(r_zoom, x_zoom, legend=false, ms=0.5, color=:black, dpi=400, xlabel=L"r", ylabel=L"x", 
     title=L"Logistic\ Map\ :\ x_n_+_1\ =\ 4rx_n(1-x_n),\ 0.7<r<1.0")
 savefig("../../computational_physics/PSet9/Figs/Zoomed_Bifurcation.pdf")
+####Part C####
+####Feigenbaum constants#####
+
+#### σ ####
+rb= collect(range(0.85, 0.99, step=10^(-4)))
+x₀=rand(range(0.88, 0.99, length=256), 256)
+x=zeros(length(rb), length(x₀))
+for (i,r) in ProgressBar(enumerate(rb))
+    x[i,:]=LogisticMap(x₀, r, 10^4 )
+end
+x
+dₙ₋₂_index = findfirst(i->length(unique(round.(x[i, :], digits=9))) == 4, 1:1401)
+dₙ₋₁_index = findfirst(i->length(unique(round.(x[i, :], digits=9))) == 8, 1:1401)
+dₙ_index = findfirst(i->length(unique(round.(x[i, :], digits=9))) == 16, 1:1401)
+δ=(rb[dₙ₋₁_index]-rb[dₙ₋₂_index])/(rb[dₙ_index]-rb[dₙ₋₁_index])
+# σ ∼ 5.0999
+#### α ####
+dₙ₊₁_index = findfirst(i->length(unique(round.(x[i, :], digits=9))) == 32, 1:1401)
+rₙ₋₂_index = findfirst(i-> 1/2 ∈ unique(ceil.(x[i, :], digits=3)), dₙ₋₂_index:dₙ₋₁_index) + dₙ₋₂_index
+rₙ₋₁_index = findfirst(i-> 1/2 ∈ unique(ceil.(x[i, :], digits=3)),dₙ₋₁_index:dₙ_index) + dₙ₋₁_index
+rₙ_index = findfirst(i-> 1/2 ∈ unique(ceil.(x[i, :], digits=3)), dₙ_index:dₙ₊₁_index) + dₙ_index
+xsₙ₋₂ = sort(unique(round.(x[rₙ₋₂_index, :], digits=3)))
+xsₙ₋₁ = sort(unique(round.(x[rₙ₋₁_index, :], digits=3)))
+xsₙ = sort(unique(round.(x[rₙ_index, :], digits=3)))
+α = (xsₙ₋₁[2] - xsₙ₋₁[1] - (xsₙ₋₂[4]- xsₙ₋₂[3] ))/( xsₙ[2] - xsₙ[1] - (xsₙ₋₁[2] - xsₙ₋₁[1]))
